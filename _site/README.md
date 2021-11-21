94f02ccfe1066dd7e05ee052a993a93c5d11e0d1
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-11-21 14:18:15 +0000

    first commit

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# av82 studio
---
+++ _site/assets/js/scripts.js
@@ -0,0 +1,5 @@
+$('.hamburger').click(function() {
+    $('.hamburger').toggleClass("is-active");
+    $('.hamburger-inner').toggleClass("dark-mode");
+    $('.navbar-container').toggleClass('navbar-open');
+});
